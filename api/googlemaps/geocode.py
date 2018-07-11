7e0b7bd777c76718fc9d3ad5d165b64d04f542e0
raiff <rmello@lenovo.com>
2018-07-11 10:34:13 -0300

    lesson 17

--- api/googlemaps/geocode.py
+++ api/googlemaps/geocode.py
@@ -5,7 +5,7 @@ import json
 def getGeocodeLocation(inputString):
     # Use Google Maps to convert a location into Latitute/Longitute coordinates
     # FORMAT: https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=API_KEY
-    google_api_key = "AIzaSyCdoyrmoiyigL2AO60Qrrk3FAxPl4Y3Xr4"
+    
     locationString = inputString.replace(" ", "+")
     print(locationString)
     url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
